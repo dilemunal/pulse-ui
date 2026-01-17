bb4ece13272ff91edc2bf4f0dca8f3b302b559e2
Dilem <diilemunal@gmail.com>
2026-01-17 06:02:28 +0300

    Initial Commit

---
+++ pulse-ui/pulse_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulse_uiApp.swift
+//  pulse-ui
+//
+//  Created by Dilem Ünal on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct pulse_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
